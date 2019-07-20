a23242a87062d21bd42d9ede499f7bb38521cf09
Gauravsharma-baseline <52987799+Gauravsharma-baseline@users.noreply.github.com>
2019-07-20 11:19:10 +0530

    Update zoho_webhook.php

--- zoho_webhook.php
+++ zoho_webhook.php
@@ -2,4 +2,7 @@
 $data = file_get_contents("php://input");
 file_put_contents('zoho1.txt',$data)  ;
 file_put_contents('zoho.txt',$_REQUEST)  ;
+file_put_contents('zoho.txt',$_POST)  ;
+file_put_content('test.txt','gjfhef');
+
 ?>
